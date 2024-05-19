d560fa4c4602bae888849a6668f38f65c572f257
Nikita Kozlov <kozlovzxc@gmail.com>
2024-05-19 19:25:45 +0200

    fix httpie

--- ubuntu.sh
+++ ubuntu.sh
@@ -26,7 +26,7 @@ install_zellij() {
     mkdir -p /tmp/zellij
     (cd /tmp/zellij && 
         echo "Downloading $URL ..." &&
-        http --follow --download "$URL" &&
+        http --follow --download --ignore-stdin "$URL" &&
         tar -xvf "$FILENAME" &&
         mv zellij ~/.local/bin
     )
@@ -65,7 +65,7 @@ install_fzf() {
     mkdir -p /tmp/fzf
     (cd /tmp/fzf && 
         echo "Downloading $URL ..." &&
-        http --follow --download "$URL" &&
+        http --follow --download --ignore-stdin "$URL" &&
         tar -xvf "$FILENAME" &&
         mv fzf ~/.local/bin
     )
