9c570a0b98fdf035f65c2d01749837d1adbb585a
Nikita Kozlov <kozlovzxc@gmail.com>
2024-05-19 18:34:41 +0200

    Added fzf install

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,62 +8,27 @@ setup_local_bin() {
     echo 'export PATH=$PATH:~/.local/bin' >> ~/.zshrc
 }
 
-install_httpie() {
+bootstrap_httpie() {
     if command -v http &> /dev/null; then
         echo "httpie already installed"
         return
     fi
+
     sudo apt install -y httpie
 }
 
 install_zellij() {
-    if command -v zellij &> /dev/null; then
-        echo "zellij already installed"
-        return
-    fi
-
-    # Determine the system architecture and OS
     VERSION=v0.40.1
-
-    # Define the base URL for the releases (replace with the actual URL if necessary)
     BASE_URL="https://github.com/zellij-org/zellij/releases/download/$VERSION/"
+    FILENAME="zellij-$ARCH-unknown-linux-musl.tar.gz"
+    URL="$BASE_URL/$FILENAME"
 
-    # Function to map architecture and OS to the release filename
-    get_filename() {
-        local arch=$1
-        local os=$2
-
-        if [[ "$arch" == "x86_64" && "$os" == "Darwin" ]]; then
-            echo "zellij-x86_64-apple-darwin.tar.gz"
-        elif [[ "$arch" == "aarch64" && "$os" == "Darwin" ]]; then
-            echo "zellij-aarch64-apple-darwin.tar.gz"
-        elif [[ "$arch" == "x86_64" && "$os" == "Linux" ]]; then
-            echo "zellij-x86_64-unknown-linux-musl.tar.gz"
-        elif [[ "$arch" == "aarch64" && "$os" == "Linux" ]]; then
-            echo "zellij-aarch64-unknown-linux-musl.tar.gz"
-        else
-            echo "Unsupported architecture or OS"
-            exit 1
-        fi
-    }
-
-    # Get the appropriate filename
-    FILENAME=$(get_filename "$ARCH" "$OS")
-
-    # Download the file
-    if [[ "$FILENAME" != "Unsupported architecture or OS" ]]; then
-        mkdir -p /tmp/zellij
-        URL="$BASE_URL/$FILENAME"
-        (cd /tmp/zellij && 
-            echo "Downloading $URL" &&
-            http --follow --download "$URL" &&
-            tar -xvf "$FILENAME" &&
-            mv zellij ~/.local/bin
-        )
-    else
-        echo "No suitable release found for your system."
-        exit 1
-    fi
+    mkdir -p /tmp/zellij
+    (cd /tmp/zellij && 
+        http --follow --download "$URL" &&
+        tar -xvf "$FILENAME" &&
+        mv zellij ~/.local/bin
+    )
 }
 
 configure_zellij() {
@@ -72,10 +37,58 @@ configure_zellij() {
     sed -i -e 's/\/\/ default_layout "compact"/default_layout "compact"/g' ~/.config/zellij/config.kdl
 }
 
+bootstrap_zellij() {
+    if command -v zellij &> /dev/null; then
+        echo "zellij already installed"
+        return
+    fi
+    install_zellij
+    configure_zellij
+}
+
+install_fzf() {
+    VERSION=0.52.1
+    BASE_URL="https://github.com/junegunn/fzf/releases/download/$VERSION"
+
+    get_arch() {
+        case "$ARCH" in
+            "x86_64") echo "amd64" ;;
+            "arm64" | "aarch64") echo "arm64" ;;
+            *) echo "$ARCH"
+        esac
+    }
+    ARCH=$(get_arch)
+    FILENAME="fzf-$VERSION-linux_$ARCH.tar.gz"
+    URL="$BASE_URL/$FILENAME"
+
+    mkdir -p /tmp/fzf
+    (cd /tmp/fzf && 
+        http --follow --download "$URL" &&
+        tar -xvf "$FILENAME" &&
+        mv fzf ~/.local/bin
+    )
+}
+
+configure_fzf() {
+    echo "# fzf
+source <(fzf --zsh)
+" >> ~/.zshrc
+}
+
+bootstrap_fzf() {
+    if command -v fzf &> /dev/null; then
+        echo "fzf already installed"
+        return
+    fi
+
+    install_fzf
+    configure_fzf
+}
+
 sudo apt update
 
 setup_local_bin
 
-install_httpie
-install_zellij
-configure_zellij
+bootstrap_httpie
+bootstrap_zellij
+bootstrap_fzf
